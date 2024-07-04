ef9363d790c020c14bbaa258c7061aefdb8f4b75
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-04 15:13:30 +0600

    second commit

---
+++ splice.js
@@ -0,0 +1,8 @@
+
+const friends = [12, 54, 453, 63, 78, 35, 85, 556]
+
+const partial = friends.splice(3,7)
+console.log(partial)
+console.log(friends)
+
+// splice slice er motoi but kono part alada korle real one theke vanish hoye jay
