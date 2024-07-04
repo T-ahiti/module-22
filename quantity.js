e8777038a5b5527868d36b783071f2c681d1f1a7
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-04 23:40:32 +0600

    3rd commit

--- quantity.js
+++ quantity.js
@@ -1,15 +1,16 @@
 const shoppingCart = [
-    {name:'shoe', price:1200},
-    {name:'shirt', price:2200},
-    {name:'pant', price:3700},
-    {name:'belt', price:600}
+    {name:'shoe', price:1200 , quantity: 2},
+    {name:'shirt', price:2200, quantity: 5},
+    {name:'pant', price:3700 , quantity: 4},
+    {name:'belt', price:600  , quantity: 3}
 ]
 
 function totalCost(products){
     let sum = 0
     for(let i = 0; i<products.length; i++){
         const product = products [i]
-        sum = sum + product.price
+        const productTotal = product.price * product.quantity
+        sum = sum + productTotal
         console.log(product)
     }
     return sum
