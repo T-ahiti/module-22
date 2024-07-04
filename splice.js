77a51016b9f42779ca931649136e026076bc4963
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-04 15:18:55 +0600

    third commit

--- splice.js
+++ splice.js
@@ -1,7 +1,9 @@
 
 const friends = [12, 54, 453, 63, 78, 35, 85, 556]
 
-const partial = friends.splice(3,7)
+const partial = friends.splice(3,7, 99, 23, 34, 4324, 23, 442)
+//  7 indicates koyta element ke shorabo
+// 99 theke 442 indicate kore ei elements gula  input dite hobe
 console.log(partial)
 console.log(friends)
 
