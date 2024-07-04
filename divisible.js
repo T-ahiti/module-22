4430fb6bcc8dea25754e716cc679ce9a3b12c1df
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-04 17:12:33 +0600

    3rd commit

--- divisible.js
+++ divisible.js
@@ -5,12 +5,17 @@
 
 
 for(let i=0; i<=50; i++){
-    if (i % 3 === 0){ 
+    if(i % 5 === 0 && i % 3 === 0){
+        console.log('foobar')
+    }
+    else if (i % 3 === 0){ 
          console.log('foo')
     }
     else if(i % 5 === 0){
         console.log('bar')
     }
+    
+    
     else{
         console.log(i)
     }
