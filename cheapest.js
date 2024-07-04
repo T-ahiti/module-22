7b17f769f1578ad59a1a7d87a63c79d784f5349e
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-04 20:12:40 +0600

    second commit

---
+++ cheapest.js
@@ -0,0 +1,58 @@
+
+const phones = [
+    {
+        name: 'samsung',
+        camera: 12,
+        storage: '32gb',
+        price: 36000,
+        color: 'silver'
+    },
+
+    {
+        name: 'samsung',
+        camera: 12,
+        storage: '32gb',
+        price: 26000,
+        color: 'silver'
+    },
+
+    {
+        name: 'samsung',
+        camera: 12,
+        storage: '32gb',
+        price: 36000,
+        color: 'silver'
+    }, 
+
+    {
+        name: 'samsung',
+        camera: 12,
+        storage: '32gb',
+        price: 36000,
+        color: 'silver'
+    },
+
+    {
+        name: 'samsung',
+        camera: 12,
+        storage: '32gb',
+        price: 36000,
+        color: 'silver'
+    },
+
+    {
+        name: 'samsung',
+        camera: 12,
+        storage: '32gb',
+        price: 36000,
+        color: 'silver'
+    },
+
+    {
+        name: 'samsung',
+        camera: 12,
+        storage: '32gb',
+        price: 36000,
+        color: 'silver'
+    }
+]
