6a98b209be48de226cda7fe4304bfeff2f3b07a7
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-04 16:47:42 +0600

    second commit

---
+++ divisible.js
@@ -0,0 +1,17 @@
+
+// show outpot from 1-100 but if the number is divisible by 3 then instead of the number show 'foo'
+// show outpot from 1-100 but if the number is divisible by 5 then instead of the number show 'bar'
+// show outpot from 1-100 but if the number is divisible by 3 then instead of the number show 'foobar'
+
+
+for(let i=0; i<=50; i++){
+    if (i % 3 === 0){ 
+         console.log('foo')
+    }
+    else if(i % 5 === 0){
+        console.log('bar')
+    }
+    else{
+        console.log(i)
+    }
+}
