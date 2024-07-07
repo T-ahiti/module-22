4878264afefc3a7080c9e302fee9ca92281111a7
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-07 19:33:54 +0600

    8th commit

--- prac.js
+++ prac.js
@@ -1,8 +1,19 @@
 
-
-function findAdress(object){
- return object
+let address = {
+    street : 10,
+    house  : '15A',
+    society: 'Earth Perfect'
 }
 
-const find = findAdress({street: 10, house:'15A', society: 'earth perfect'})
+function findAdress(object){
+    let values = []
+    for(let key in object){
+        if(object.hasOwnProperty(key)){
+            values.push(object[key])
+        }
+    }
+ return values.join(',')
+}
+
+const find = findAdress(address)
 console.log(find)
