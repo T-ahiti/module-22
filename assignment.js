273df51c318009537f242f76073ef57d71c37258
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-07 18:08:49 +0600

    7th commit

--- assignment.js
+++ assignment.js
@@ -43,11 +43,9 @@ function sortMaker(array){
   if(  array[0] ===array [1]){
       return 'equal';
   }
-
   else if( array[0] <=0 || array[1] <=0){
       return 'invalid input'
   }
-
   else if( array[0] >=0 && array[1] >=0){
     let max = Math.max(array[0], array[1])
     let min = Math.min(array[0], array[1])
@@ -55,9 +53,7 @@ function sortMaker(array){
     array[1] = min
     return array
   }
-
 }
-
 const maker = sortMaker([2, 4]);
-console.log(maker)
+// console.log(maker)
 
--- prac.js
+++ prac.js
@@ -1,24 +1,8 @@
 
 
-
-function sortMaker(array){
-    if(  array[0] ===array [1]){
-        return 'equal';
-    }
-
-    else if( array[0] <=0 || array[1] <=0){
-        return 'invalid input'
-    }
-
-    else if( array[0] >=0 && array[1] >=0){
-      let max = Math.max(array[0], array[1])
-      let min = Math.min(array[0], array[1])
-      array[0] = max
-      array[1] = min
-      return array
-    }
-
+function findAdress(object){
+ return object
 }
 
-const maker = sortMaker([2, 4]);
-console.log(maker)
+const find = findAdress({street: 10, house:'15A', society: 'earth perfect'})
+console.log(find)
