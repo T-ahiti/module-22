381633e59b2b685c1a75b638223edfd9b94f164e
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-07 20:29:45 +0600

    11th commit

--- assignment.js
+++ assignment.js
@@ -77,3 +77,27 @@ return values.join(',')
 }
 const find = findAdress(address)
 // console.log(find)
+
+
+
+
+
+
+let array = [1, 2, 5]
+function canPay(give , price){
+   if(array.length === 0){
+    return 'please give a proper array'
+   }
+   let sum  = 0
+    for(let i =0 ; i<array.length; i++){
+        sum += array[i];
+   }
+    if(sum >= price){
+       return true;
+   }
+   else{
+    return false;
+   }
+}
+const pay = canPay(array, 10)
+// console.log(pay)
--- prac.js
+++ prac.js
@@ -1,10 +1,20 @@
 
 
 let array = [1, 2, 5]
-
 function canPay(give , price){
-
+   if(array.length === 0){
+    return 'please give a proper array'
+   }
+   let sum  = 0
+    for(let i =0 ; i<array.length; i++){
+        sum += array[i];
+   }
+    if(sum >= price){
+       return true;
+   }
+   else{
+    return false;
+   }
 }
-
 const pay = canPay(array, 10)
-console.log(pay)
+// console.log(pay)
